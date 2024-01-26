fd5330679be984d709116ce3a5398f678e1c4988
Mani037 <mani334422@gmail.com>
2024-01-26 16:23:56 +0530

    mern blog

--- client/src/components/Footers.jsx
+++ client/src/components/Footers.jsx
@@ -20,7 +20,7 @@ const Footers = () => {
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white ">
-                Sahand's
+                MERN
               </span>
               Blog
             </Link>
@@ -41,7 +41,7 @@ const Footers = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Sahand's Blog
+                  MERN Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -79,7 +79,7 @@ const Footers = () => {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href="#"
-            by="sahand's blog"
+            by="MERN blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-4 sm:mt-0  mt-4 md:justify-center items-center">
